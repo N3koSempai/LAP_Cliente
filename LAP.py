8c978b5c6eb12b633ca9516265f949cfb14aa9ef
N3koSempai <alvarom12098@gmail.com>
2023-02-12 18:55:45 -0500

    change name

---
+++ LAP.py
@@ -0,0 +1,12 @@
+from modulos.repoadm import repoAdm
+import sys
+
+repo = repoAdm()
+
+if sys.argv[1] == 'actualizar':
+    repo.getrepo()
+elif sys.argv[1] == 'instalar':
+    if sys.argv[2] != None:
+        repo.jsonSearch(sys.argv[2])
+    else:
+        print('escribe algun paquete')
