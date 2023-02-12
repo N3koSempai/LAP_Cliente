aeae4fb2a46ccf2577364dc7343b6ea18cae8ede
N3koSempai <alvarom12098@gmail.com>
2023-02-12 04:34:10 -0500

    added coment to modules

--- modulos/repoadm.py
+++ modulos/repoadm.py
@@ -9,15 +9,23 @@ class repoAdm():
     
     
     def __init__(self):
-        self.url = 'https://github.com/N3koSempai/LAP_Directory/archive/refs/heads/main.zip'
         # definiendo el origen de los repositorios
+        self.url = 'https://github.com/N3koSempai/LAP_Directory/archive/refs/heads/main.zip'
+        
     def getrepo(self):
+        # estableciendo directorio actual que es dentro de modulos
         chdir(os.path.dirname(os.path.realpath(__file__)))
+        
+        
         try:
+            # descargando lista de librerias en formato zip
             response = urllib.request.urlopen(self.url)
             data = response.read()
+            # creando directorio para repo
             mkdir('../repo')
+            
         except OSError as e:
+            #capturando excepcion en caso de que exista el directorio
             if e.errno != errno.EEXIST:
                 print(e)
             else:
@@ -25,11 +33,15 @@ class repoAdm():
         except:
             print("error")
         finally:
+            # verificando respuesta correcta del servidor
             if response.status == 200:    
                 with open('../repo/main.zip', 'wb') as compress:
+                    #creando comprimido
                     compress.write(data)
                 zipx = ZipFile('../repo/main.zip', 'r')
+                # extrayendo
                 zipx.extractall('../repo/')
+                #ajustando carpetas y borrando temporales
                 shutil.move('../repo/LAP_Directory-main/main.json', '../repo/main.json')
                 shutil.rmtree('../repo/LAP_Directory-main')
                 os.remove('../repo/main.zip')
